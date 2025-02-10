53d0a9866bdf4219a28e369a1a511c3535dd7df6
MaHuAkash <aakashlamsal099@gmail.com>
2025-02-10 00:32:28 -0800

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,88 @@
+var youtube = "404";
+var twitter = "404";
+var password = "Minaraju1212@";
+var linkedin = "404";
+var instagram = "404";
+var github = "afno profile ko urlllll";
+var email = 'hadagood4@gmail.com';
+
+whois = [
+  "<br>",
+  "Yo, I’m Akash, your friendly neighborhood procrastinator. 🕸️",
+  "Once went swimming in the Ninda river… during a flood. Not smart, but definitely iconic. 🌊",
+  "They say I’m wild, but plot twist: I’m just a good boy with questionable hobbies. 😇",
+  "Dal Bhat Masu is my religion. Serve me that, and I’ll code you anything. 🍛",
+  "Call me infinity, because I’m stuck somewhere between 0 and 1. ♾️",
+  "The Matrix is my dream address. I’m Neo, but I spend more time debugging than saving humanity. 🕶️💻",
+  "IDE + cigarette = my ultimate zen. Forget yoga, this is mindfulness. 🚬",
+  "Emoji game? Out of this world. Like literally, 🌌 is my spirit animal.",
+  "You think I’m lazy? Nah, I’m energy-efficient. ♻️",
+  "Coding is my jam, but guitar is my backup plan in case the bugs win. 🎸",
+  "Hobbies? Making life harder for myself just to laugh at it later. 😂",
+  "They say I’m nerdy. I say I’m just living life one algorithm at a time. 📚",
+  "Socializing? Nah, I’m busy method-overloading my life with randomness. 🤓",
+  "Once tried to explain infinity to my friends. They now avoid me. 😅",
+  "Wild outside, calm inside. Basically, I’m Dalai Lama in debug mode. 🧘‍♂️",
+  "If bad habits were a sport, I’d be MVP. But hey, at least I’m consistent. 🏆",
+  "People say I’m hard to read. I just think they lack the proper library to parse me. 📖",
+  "Welcome to my life—a chaotic mix of Dal Bhat, IDEs, and questionable choices. Enjoy! 🌀",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/akassh' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/akash' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/akash' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/akash' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/akash' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is akash?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+ banner = [
+
+  
+  
+  "     ░▒▓██████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░  ░▒▓██████▓▒░   ░▒▓███████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ",
+  "    ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ ",
+  "    ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░        ░▒▓█▓▒░░▒▓█▓▒░ ",
+  "    ░▒▓████████▓▒░ ░▒▓███████▓▒░  ░▒▓████████▓▒░  ░▒▓██████▓▒░  ░▒▓████████▓▒░ ",
+  "    ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░        ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ",
+  "    ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░        ░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ",
+  "    ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░░▒▓█▓▒░ ░▒▓███████▓▒░  ░▒▓█▓▒░░▒▓█▓▒░ ",
+  "                                                                               ",
+  "                                                                               "
+];
